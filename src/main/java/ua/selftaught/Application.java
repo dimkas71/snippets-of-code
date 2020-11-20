0560ae892251e29085c6c7d921704d51158049d1
dimkas71g@gmail.com <dimkas71g@gmail.com>
2020-11-20 15:59:50 +0200

    add fragments thymeleaf support

--- src/main/java/ua/selftaught/Application.java
+++ src/main/java/ua/selftaught/Application.java
@@ -1,10 +1,24 @@
 package ua.selftaught;
 
+import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.runtime.Micronaut;
+import io.micronaut.views.View;
 
+@Controller("/")
 public class Application {
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
     }
+
+    @View("home")
+    @Get()
+    public HttpResponse index(@QueryValue("name") String name) {
+        return HttpResponse.ok(CollectionUtils.mapOf("name", name));
+    }
+
 }
